17b0340ea3088506da26c161d4f2d7bcc5601aae
HemanthKumarMachani <hemanthmachani15@gmail.com>
2025-04-29 11:30:49 -0400

    helm chart upgrade temp fix

--- src/main/java/org/initial/basicgamearena/service/PlayerService.java
+++ src/main/java/org/initial/basicgamearena/service/PlayerService.java
@@ -1,6 +1,8 @@
 package org.initial.basicgamearena.service;
 
 import org.initial.basicgamearena.model.Player;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -8,11 +10,12 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class PlayerService {
-
+    private static final Logger logger = LoggerFactory.getLogger(PlayerService.class);
     private final Map<String, Player> players = new ConcurrentHashMap<>();
 
     public Player registerPlayer(Player player) {
         players.put(player.getId(), player);
+        logger.info("New player registered: {}" , player);
         return player;
     }
 
